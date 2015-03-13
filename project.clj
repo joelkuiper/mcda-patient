34893f43acae7407cb4511faf656e4acc23c39e7
Joel Kuiper <me@joelkuiper.eu>
2015-03-13 15:23:41 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,26 +4,26 @@
   :url "http://drugis.org"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring-server "0.3.1"]
-                 [selmer "0.8.0"]
-                 [com.taoensso/timbre "3.3.1"]
+                 [ring-server "0.4.0"]
+                 [selmer "0.8.2"]
+                 [com.taoensso/timbre "3.4.0"]
                  [markdown-clj "0.9.64"]
                  [environ "1.0.0"]
                  [im.chit/cronj "1.4.3"]
                  [cheshire "5.4.0"]
                  [compojure "1.3.2"]
-                 [ring/ring-defaults "0.1.3"]
+                 [ring/ring-defaults "0.1.4"]
                  [ring/ring-session-timeout "0.1.0"]
                  [ring-middleware-format "0.4.0"]
                  [noir-exception "0.2.3"]
                  [bouncer "0.3.2"]
-                 [lib-noir "0.9.0"]
-                 [prone "0.8.0"]
-                 [buddy "0.3.0"]
+                 [lib-noir "0.9.5"]
+                 [prone "0.8.1"]
+                 [buddy "0.4.1"]
                  [ragtime "0.3.8"]
                  [yesql "0.5.0-rc1"]
                  [crypto-random "1.2.0"]
-                 [com.h2database/h2 "1.4.182"]]
+                 [com.h2database/h2 "1.4.186"]]
 
   :min-lein-version "2.0.0"
   :uberjar-name "mcda-patient.jar"
@@ -57,7 +57,7 @@
                                  :auto-reload?  false}}
              :dev {:dependencies [[ring-mock "0.1.5"]
                                   [ring/ring-devel "1.3.2"]
-                                  [pjstadig/humane-test-output "0.6.0"]]
+                                  [pjstadig/humane-test-output "0.7.0"]]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
                    :env {:dev true}}})
