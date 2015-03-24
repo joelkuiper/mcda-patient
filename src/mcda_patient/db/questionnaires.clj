a6401216204ad4d5f770d307e59b3d9ece51a6cb
Joel Kuiper <me@joelkuiper.eu>
2015-03-24 22:39:18 +0100

    Switch from CLOB to VARCHAR
    - Limit in H2 is 2Gb

--- src/mcda_patient/db/questionnaires.clj
+++ src/mcda_patient/db/questionnaires.clj
@@ -12,24 +12,19 @@
 
 (timbre/refer-timbre)
 
-(defn clob-to-string [clob]
-  (when-not (nil? clob)
-    (with-open [rdr (java.io.BufferedReader. (.getCharacterStream clob))]
-      (apply str (line-seq rdr)))))
-
 (defn random-url [] (crypto.random/url-part 5))
 
 (defn query [] (query-questionnaires))
 
-(defn with-decoded-problem-clob
+(defn with-decoded-problem
   [questionnaire]
-  (assoc questionnaire :problem (parse-string (clob-to-string (:problem questionnaire)))))
+  (update-in questionnaire :problem parse-string))
 
 (defn get
   ([id] (jdbc/with-db-transaction [tx db-spec] (get id tx)))
   ([id tx]
    (when-let [questionnaire (first (get-questionnaire {:id id} {:connection tx}))]
-     (with-decoded-problem-clob questionnaire))))
+     (with-decoded-problem questionnaire))))
 
 (defn round-trip-json
   "This round trips the JSON, will fail if the input was not valid JSON.
@@ -40,7 +35,7 @@
 (defn create!
   [title problem num-urls]
   (let [id (first (vals (create<! {:title title :problem (round-trip-json problem)})))
-        urls (map vector (repeat id) (take num-urls (repeatedly random-url)))]
+        urls (map vector (repeat id) (repeatedly num-urls random-url))]
     (jdbc/with-db-transaction [tx db-spec]
       (doall
        (map (fn [[id url]] (insert-result! {:id id :url url} {:connection tx})) urls)))
@@ -58,7 +53,7 @@
   (jdbc/with-db-transaction [tx db-spec]
     (let [questionnaire (first (get-questionnaire-by-url {:url url} {:connection tx}))]
       (visit-questionnaire! {:url url} {:connection tx})
-      (with-decoded-problem-clob questionnaire))))
+      (with-decoded-problem questionnaire))))
 
 (defn save-result!
   [url answers]
@@ -69,7 +64,8 @@
   (jdbc/with-db-transaction [tx db-spec]
     (let [results (get-results-by-id {:id id} {:connection tx})]
       (doall
-       (map (fn [result] (assoc result :answers (parse-string (clob-to-string (:answers result))))) results)))))
+       (map (fn [result]
+              (update-in result :answers parse-string)) results)))))
 
 (defn get-urls
   [id]
